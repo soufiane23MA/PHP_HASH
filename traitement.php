f706c928cc7b7f078137ba119051e89372f006ac
soufiane23Ma <soufiane.elmaarouf@gmail.com>
2024-12-10 14:21:55 +0100

    PHP_HASH

--- traitement.php
+++ traitement.php
@@ -1,80 +1,77 @@
 <?php
- 
+
 
 if(isset($_GET["action"])){
 	switch ($_GET["action"]){
 		case "register":
-				//se connecter à la base de donnée, qu'on créra en avance)
-				//dsn, username et le password.
-				// si l'utilisateur a soumis son formulaire 
+			//se connecter à la base de donnée, qu'on créra en avance)
+			//dsn, username et le password.
+			// si l'utilisateur a soumis son formulaire 
 			if($_POST["submit"]){
-			$pdo = new PDO("mysql:host=localhost;dbname=php_hash;charset=utf8","root","");
+				$pdo = new PDO("mysql:host=localhost;dbname=php_hash;charset=utf8","root","");
 				// filtrer la data utilisateur récuperées dans les  champs inputs.
-			$pseudo =filter_input(INPUT_POST,"pseudo",FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-			$email =filter_input(INPUT_POST,"email",FILTER_SANITIZE_EMAIL,FILTER_VALIDATE_EMAIL);
-			$pass1 =filter_input(INPUT_POST,"pass1",FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-			$pass2 =filter_input(INPUT_POST,"pass2",FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-			
-			if($pseudo && $email && $pass1 && $pass2){
-				//var_dump($email);die;
-				// aprés virification que les filter marchent, 
-				$requete = $pdo->prepare("SELECT * FROM user WHERE email = :email");
-				$requete ->execute(["email"=>$email]);
-				$user = $requete->fetch();
-				//si l'utilisateur existe dans notre base de donées		
-						if($user){
-							//echo "bonjour user";
-							header("location:register.php"); exit;
-						} else {
-							//var_dump("utilisateur non existant");
-							//echo " cet email , n existe pas";
-							//ça marche
-							// là maintenat je verifie si les 2 mots de passe 
-							// sont identique , et
+				$pseudo =filter_input(INPUT_POST,"pseudo",FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+				$email =filter_input(INPUT_POST,"email",FILTER_SANITIZE_EMAIL,FILTER_VALIDATE_EMAIL);
+				$pass1 =filter_input(INPUT_POST,"pass1",FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+				$pass2 =filter_input(INPUT_POST,"pass2",FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+				if($pseudo && $email && $pass1 && $pass2){
+					var_dump($email);die;
+					// aprés virification que les filter marchent, 
+					$requete = $pdo->prepare("SELECT * FROM user WHERE email = :email");
+					$requete ->execute(["email"=>$email]);
+					$user = $requete->fetch();
+					//si l'utilisateur existe dans notre base de donées		
+					if($user){
+						//echo "bonjour user";
+						header("location:register.php"); exit;
+					}else {
+						//var_dump("utilisateur non existant");
+						//echo " cet email , n existe pas";
+						//ça marche
+						// là maintenat je verifie si les 2 mots de passe 
+						// sont identique , et
 						if($pass1 == $pass2 && strlen($pass1) >= 5){
 							// on va inserer l'utilisateur dans la base de données,
 							// avec une requête préparée
 							$insertUser = $pdo ->prepare("INSERT INTO user (pseudo,email,password) 
-								VALUES (:pseudo,:email,:password)");
+							VALUES (:pseudo,:email,:password)");
 							$insertUser->execute([
 								"pseudo"=>$pseudo,
 								"email"=>$email,
-							// attention le mot de passe doit être hasher: 
-							//pour ne pas avoir une emprunt numerique en claire dans la base de donnée.
-								"password"=> password_hash($pass1,PASSWORD_DEFAULT)
-							]);
-									header("location:login.php"); exit;
+								// attention le mot de passe doit être hasher: 
+								//pour ne pas avoir une emprunt numerique en claire dans la base de donnée.
+								"password"=> password_hash($pass1,PASSWORD_DEFAULT)]);
+								header("location:login.php"); exit;
 							}else{
 								// message pour informer l'utilisateur que 
 								//les 2 mots de passe ne sont pas identiques
-							}
-							}
-						}else{
-							//problème de saisi 
-						}
-						header("location:register.php");exit;
-					}// je redirige l'utilisateur vers le formulaire pour s'enregister
-					
-			break;
-			case "login":
-				
-				if($_POST["submit"]){
-					//on se concécte à la base de données
-					$pdo = new PDO("mysql:host=localhost;dbname=php_hash;charset=utf8","root","");
-					//on filtre les data utilisateur soumise dans le formulaire
-					$email =filter_input(INPUT_POST,"email",FILTER_SANITIZE_EMAIL,FILTER_VALIDATE_EMAIL);
-					$password=filter_input(INPUT_POST,"password",FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-					//si on a éffectivement une adresse mail et un mot de passe
-					//on prépare une requête pour récuperer la data utilisateur de la base de données
-					//pour la comparer a celle qui vien de soumis.
-					//vérification de l'empreinte numérique via password_verify() qui returne false or true
-					if($email && $password){
-						var_dump("ok");die;
-						$requete = $pdo->prepare("SELECT * FROM user WHERE email=:email");
-						$requete ->execute(["email"=> $email]);
-						$user = $requete->fetch();// récupere le resultat de la requête et la stockée dans la variable user 
-				    //var_dump($user);die;
-					}if($user){
+								}
+					}
+				}else{
+					//problème de saisi 
+					}
+					header("location:register.php");exit;
+					// je redirige l'utilisateur vers le formulaire pour s'enregister
+			}
+		break;
+		case "login":
+			if($_POST["submit"]){
+				//on se concécte à la base de données
+				$pdo = new PDO("mysql:host=localhost;dbname=php_hash;charset=utf8","root","");
+				//on filtre les data utilisateur soumise dans le formulaire
+				$email =filter_input(INPUT_POST,"email",FILTER_SANITIZE_EMAIL,FILTER_VALIDATE_EMAIL);
+				$password=filter_input(INPUT_POST,"password",FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+				//si on a éffectivement une adresse mail et un mot de passe
+				//on prépare une requête pour récuperer la data utilisateur de la base de données
+				//pour la comparer a celle qui vien de soumis.
+				//vérification de l'empreinte numérique via password_verify() qui returne false or true
+				if($email && $password){
+					//var_dump("ok");die;
+					$requete = $pdo->prepare("SELECT * FROM user WHERE email=:email");
+					$requete ->execute(["email"=> $email]);
+					$user = $requete->fetch();// récupere le resultat de la requête et la stockée dans la variable user 
+					var_dump($user);die;
+					if($user){
 						$hash =$user["password"];
 						if(password_verify($password,$hash)){
 							$_SESSION["user"] = $user;
@@ -84,19 +81,12 @@ if(isset($_GET["action"])){
 							echo"identifiant non enregistrer";
 						}
 					}
-					}
-					header("location:login.php");
-					exit;
-					
+				}
+				header("location:login.php");
+				exit;
+			}
 			break;
-			
-			};
-		}
-		
-
-	
+	}
+};
 
 
-	
-
-	
